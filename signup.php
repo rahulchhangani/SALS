16291acf0af79b20c14f093bc0b95cf5be06317e
Vaishno Chaitanya <vaishnochaitanya7@gmail.com>
2016-10-28 20:02:59 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,3 +1,10 @@
+/********************************************************************************************************************
+* This script is brought to you by mstrhak0r by whom all copyrights are reserved.
+* Website: www.facebook.com/mstrhak0r
+* Email: mstrhak0r@gmail.com
+* Please, do not remove this information from the top of this page.
+*********************************************************************************************************************/
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
